f488fccadd4f893e4b243d107f34c4caf1afa71e
Hyeonsu Lee <incleaf@gmail.com>
2016-05-26 02:11:56 +0900

    Update font weight immediately when slider updated

--- app/inject.js
+++ app/inject.js
@@ -1,6 +1,11 @@
-chrome.storage.sync.get([
-  'webkitTextStroke', 'webkitFontSmoothing'
-], function(items) {
-  document.body.style.webkitTextStroke = items.webkitTextStroke || '0.25px';
-  document.body.style.webkitFontSmoothing = items.webkitFontSmoothing || 'antialiased';
-});
+(function() {
+  chrome.storage.sync.get([
+    'webkitTextStroke'
+  ], function(items) {
+    document.body.style.webkitFontSmoothing = 'antialiased';
+    document.body.style.webkitTextStroke = items.webkitTextStroke
+      ? (items.webkitTextStroke / 100) + 'px'
+      : '0.25px';
+  });
+})();
+
--- app/options.js
+++ app/options.js
@@ -1,30 +1,51 @@
-var slider = document.getElementById('slider');
+(function() {
+  chrome.storage.sync.get([
+    'webkitTextStroke'
+  ], function(items) {
+    var savedValue = items.webkitTextStroke || 25;
+    var slider = document.getElementById('slider');
+    var resetBtn = document.getElementById('resetBtn');
+    var doneBtn = document.getElementById('doneBtn');
+    var isSliding;
 
-noUiSlider.create(slider, {
-  start: 25,
-  connect: "lower",
-  pips: {
-    mode: 'values',
-    values: [0, 10, 20, 30, 40, 50],
-    density: 4
-  },
-  range: {
-    min: 0,
-    max: 50
-  }
-});
+    noUiSlider.create(slider, {
+      start: savedValue,
+      connect: "lower",
+      pips: {
+        mode: 'values',
+        values: [0, 10, 20, 30, 40, 50],
+        density: 4
+      },
+      range: {
+        min: 0,
+        max: 50
+      }
+    });
 
-function save_options() {
-  // Saves options to chrome.storage.sync.
-  chrome.storage.sync.set({
-    webkitTextStroke: '0.5px'
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+    slider.noUiSlider.on('update', function() {
+      if (isSliding) {
+        clearInterval(isSliding);
+      }
+      isSliding = setTimeout(function() {
+        chrome.storage.sync.set({
+          webkitTextStroke: slider.noUiSlider.get()
+        }, function() {
+          chrome.tabs.executeScript({
+            file: 'inject.js'
+          });
+          isSliding = null;
+        });
+      }, 100);
+    });
+
+    resetBtn.addEventListener('click', function() {
+      slider.noUiSlider.set(25);
+    });
+
+    doneBtn.addEventListener('click', function() {
+      window.close();
+    });
+
   });
-}
-document.getElementById('save').addEventListener('click', save_options);
+})();
+
